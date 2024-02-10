cac6383f9b3ecce34ba53dd82b6e585a5e2a8d79
InvertedOwl <70726294+InvertedOwl@users.noreply.github.com>
2024-02-10 08:41:38 -0700

    Mega improvement

--- index.js
+++ index.js
@@ -3,25 +3,58 @@ const fetch = require('node-fetch');
 const sqlite3 = require('sqlite3').verbose();
 const app = express()
 app.use(express.static('maninchair/dist'));
+const fs = require('fs');
 
 const port = 3101
 
 // Connect to the database
-let db = new sqlite3.Database('stats.db', (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQLite database.');
+function createDB(name) {
+    fs.mkdir("dbs", () => {});
+    let db = new sqlite3.Database("dbs/" + name + '.db', (err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+            
+    });
     db.run("CREATE TABLE IF NOT EXISTS matches (rowid INTEGER PRIMARY KEY, modified INTEGER, deleted INTEGER, sku TEXT, division INTEGER, round TEXT, instance INTEGER, match INTEGER, data TEXT);");
     db.run("CREATE TABLE IF NOT EXISTS rankings (rowid INTEGER PRIMARY KEY, modified INTEGER, deleted INTEGER, sku TEXT, division INTEGER, round TEXT, teamnum TEXT, rank INTEGER, wp INTEGER, ap INTEGER, sp INTEGER, wins INTEGER, losses INTEGER, ties INTEGER, winPct REAL, numMatches INTEGER, totalPoints INTEGER, avgPoints REAL, highScore INTEGER);");
     db.run("CREATE TABLE IF NOT EXISTS stats (rowid INTEGER PRIMARY KEY,modified INTEGER,deleted INTEGER,sku TEXT,division INTEGER,teamnum TEXT,opr REAL,dpr REAL,ccwm REAL);");
     db.run("CREATE TABLE IF NOT EXISTS skills ( rowid INTEGER PRIMARY KEY, modified INTEGER, deleted INTEGER, sku TEXT, teamnum TEXT, rank INTEGER, tie INTEGER, ageGroup TEXT, totalScore INTEGER, driverAttempts INTEGER, driverHighScore INTEGER, progAttempts INTEGER, progHighScore INTEGER);");
+    console.log('Created DB for ' + name);
 
-    
-    
-});
+    return db;
+}
 
-function sqliteToJson(tableName, callback) {
+
+const lastRequest = {}
+let toUpdate = []
+
+function updateData() {
+
+    set = [...new Set(toUpdate)];
+    set.forEach((event) => {
+        db = lastRequest[event].db;
+        fetch("https://data.vexvia.dwabtech.com/api/v3/event/"+event+"?schema=212&since=" + 0 + "&timeout=100000").then((response) => {
+            response.text().then((text) => {
+                console.log("Got event data for " + event);
+                db.run("BEGIN TRANSACTION", (err) => {if (!err)return;console.log("Begin err " + err)});
+    
+                for (let i = 0; i < text.split(";").length; i++) {
+                    db.run(text.split(";")[i], (err) => {
+    
+                    });
+                }
+                db.run("COMMIT", (err) => {if (!err)return; console.log("Commit err " + err) });
+            })
+            console.log("Finished");
+        });
+    }) 
+    toUpdate = [];
+}
+
+setInterval(updateData, 2000);
+
+function sqliteToJson(tableName, db, callback) {
     db.all(`SELECT * FROM ${tableName}`, [], (err, rows) => {
         if (err) {
             console.error(err.message);
@@ -37,34 +70,28 @@ app.get('/data/:type', (req, res) => {
         return;
     }
     try {
-        console.log(req.query.event.toLowerCase());
-        fetch("https://data.vexvia.dwabtech.com/api/v3/event/"+req.query.event.toLowerCase()+"?schema=212&since=0&timeout=100000").then((response) => {
-            response.text().then((text) => {
-                const type = req.params.type; // Access the variable part
-                db.run("BEGIN TRANSACTION", (err) => {console.log("Begin err " + err)});
-                db.run("DELETE FROM matches", (err) => {console.log("Matches err " + err)});
-                db.run("DELETE FROM rankings", (err) => {console.log("Rankings err " + err)});
-                db.run("DELETE FROM stats", (err) => {console.log("Stats err " + err)});
-                db.run("DELETE FROM skills", (err) => {console.log("Skills err " + err)});            
-                for (let i = 0; i < text.split(";").length; i++) {
-                    db.run(text.split(";")[i], (err) => {
-                        if (err) {
-                            console.log(text.split(";")[i]);
-                        }
-                    });
-                }
-                db.run("COMMIT", (err) => { console.log("Commit err " + err) });
-                const tableName = type; // Update with your table name
 
-                sqliteToJson(tableName, (err, jsonData) => {
-                    if (err) {
-                        res.status(500).send('Error reading database');
-                    } else {
-                        res.json(jsonData);
-                    }
-                });
-            })
-        });  
+        console.log(req.query.event.toLowerCase());
+        
+        let db;
+        if (!Object.keys(lastRequest).includes(req.query.event.toLowerCase())) {
+            db = createDB(req.query.event.toLowerCase());
+            lastRequest[req.query.event.toLowerCase()] = {db: db, since: 0};
+            updateData(req.query.event.toLowerCase());
+        } else {
+            db = lastRequest[req.query.event.toLowerCase()].db;
+        }
+        console.log(lastRequest);
+
+        sqliteToJson(req.params.type, db, (err, jsonData) => {
+            if (err) {
+                res.status(500).send('Error reading database');
+            } else {
+                res.json(jsonData);
+            }
+        });
+        toUpdate.push(req.query.event.toLowerCase());
+
     } catch (e) {
         console.log(e);
     }
